44221e82fbda3841b699f039afd3793b7bb8bee3
yogesh <ykakran05@gmail.com>
2025-05-08 08:16:26 +0530

    added header row

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -2,8 +2,20 @@
 const TransactionTable = ({ transactions }) => {
 
   return (
-    <div>
-      <h1>Transactions</h1>
+    <div className="transaction-table-container">
+      <h1 className="transaction-table-header">Transactions</h1>
+      <div className="transaction-table">
+        
+        <div className="header-row">
+          <div className="header-date-column">Date</div>
+          <div className="header-data-columns">
+            <div className="header-column">To/From</div>
+            <div className="header-column">Amount</div>
+            <div className="header-column">Account</div>
+            <div className="header-column">Payment Method</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
